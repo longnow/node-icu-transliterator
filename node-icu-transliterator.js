8ff6be7f2efcd921edf66db42a1c4b8e55eea4d9
David Kamholz <lautgesetz@gmail.com>
2018-06-12 18:13:29 -0700

    check for missing rules parameter

--- node-icu-transliterator.js
+++ node-icu-transliterator.js
@@ -1,6 +1,7 @@
 let native = require('bindings')('node-icu-transliterator');
 
 let self = module.exports = function(rules, dir) {
+    if (rules === undefined) throw 'missing rules parameter';
     if (dir === undefined) dir = self.FORWARD;
     return new native.Transliterator(rules, dir);
 }
